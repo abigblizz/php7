ab565616abefe491808bc912bb76a63afe9453f3
abigblizz <bevolover@live.com>
2017-12-18 18:35:41 -0500

    Initial php code

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Getting started with PHP</title>
+</head>
+<body>
+
+<?php
+echo '<h1>Hello World!</h1>' ;
+?>
+
+</body>
+</html>
---
+++ string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Getting started with PHP</title>
+</head>
+<body>
+
+<?php
+$phrase = 'The truth is rarely pure';
+$author = 'Oscar Wilde';
+echo $phrase;
+echo "<p>It is often said that <q>$phrase</q> </p>";
+$phrase = $phrase . 'and never simple';
+echo "<p><q>$phrase</q><cite> $author</cite></p>";
+?>
+
+</body>
+</html>
